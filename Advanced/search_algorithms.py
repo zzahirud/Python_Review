0f8f17148d38032959381c0cbb07bde7121a4a31
Zubair Zahiruddin <contactzuby@gmail.com>
2017-03-29 11:12:39 -0400

    Update search_algorithms.py

--- Advanced/search_algorithms.py
+++ Advanced/search_algorithms.py
@@ -18,3 +18,29 @@ print(sequentialSearch(testlist, 13))
 
 
 #binary search
+
+testlist = [0, 1, 2, 8, 13, 17, 19, 32, 42]
+#print(binarySearch(testlist, 3))
+#print(binarySearch(testlist, 13))
+
+def my_search (l, item):
+    l.sort()
+    first_index = 0
+    last_index = len(l)-1
+    found = False
+
+    while first_index <= last_index and not found :
+
+        if l[last_index//first_index] == item:
+            found = True
+        else:
+            if item > l[(last_index//first_index)]:
+                first_index = (last_index//first_index)
+            elif item < l[(last_index//first_index)]:
+                last_index = (last_index//first_index)
+
+    return found
+
+print my_search(testlist,3)
+# 0(n) = log
+O(logn)
