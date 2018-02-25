83531b2a4d107413f41bb2c8c1bbac39a409b605
zubair <contactzuby@gmail.com>
2018-02-24 20:02:34 -0500

    parChecker

--- DB/stacks.py
+++ DB/stacks.py
@@ -39,3 +39,37 @@ def revstring(mystr):
         s.pop()
     return  rev
 print(revstring("zubair"))
+
+
+
+#  Closing symbols match opening symbols in the reverse order of their appearance;
+# they match from the inside out. This is a clue that stacks can be used to solve the problem.
+
+#Use a queue when you want to get things out in the order that you put them in.
+#Use a stack when you want to get things out in the reverse order than you put them in.
+#Use a list when you want to get anything out, regardless of when you put them in (and when you don't want them to automatically be removed).
+
+def parChecker(symbolString):
+    s = Stack()
+    balanced = True
+    index = 0
+    while index < len(symbolString) and balanced:
+        symbol = symbolString[index]
+        if symbol == "(":
+            s.push(symbol)
+        else:
+            if s.isEmpty():
+                balanced = False
+            else:
+                s.pop()
+
+        index = index + 1
+
+    if balanced and s.isEmpty():
+        return True
+    else:
+        return False
+
+print(parChecker('((()))'))
+print(parChecker('(()'))
+
