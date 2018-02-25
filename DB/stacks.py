f55b049074dcca4b9cd3e77e86c0057cafe3eb2f
zubair <contactzuby@gmail.com>
2018-02-24 19:06:15 -0500

    defination

---
+++ DB/stacks.py
@@ -0,0 +1,29 @@
+#Stack
+#LIFO
+#using a built in data structure - list and list functions.
+
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[len(self.items) - 1]
+
+    def size(self):
+        return len(self.items)
+
+s = Stack()
+s.push('hello')
+s.push('true')
+print(s.pop())
+
+print(s.items)
